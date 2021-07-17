5d720412b7e9cfb5cbed1b6363b7b3a500cb9a9e
david <davidscandurra@gmail.com>
2021-07-17 10:04:03 +0200

    Register instruction types

--- src/main/java/io/github/splotycode/hecate/instruction/Instructions.java
+++ src/main/java/io/github/splotycode/hecate/instruction/Instructions.java
@@ -1,8 +1,16 @@
 package io.github.splotycode.hecate.instruction;
 
-import io.github.splotycode.hecate.instruction.load.ALoad;
-import jdk.internal.org.objectweb.asm.tree.AbstractInsnNode;
-import jdk.internal.org.objectweb.asm.tree.VarInsnNode;
+import io.github.splotycode.hecate.instruction.flow.Compare;
+import io.github.splotycode.hecate.instruction.flow.CompareOperation;
+import io.github.splotycode.hecate.instruction.flow.Label;
+import io.github.splotycode.hecate.instruction.flow.ReturnVoid;
+import io.github.splotycode.hecate.instruction.local.Load;
+import io.github.splotycode.hecate.instruction.local.Store;
+import io.github.splotycode.hecate.instruction.stack.Push;
+import io.github.splotycode.hecate.tree.primitive.HPrimitiveType;
+import org.objectweb.asm.tree.AbstractInsnNode;
+import org.objectweb.asm.tree.IntInsnNode;
+import org.objectweb.asm.tree.VarInsnNode;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -16,23 +24,30 @@ public class Instructions {
     private static final Map<Integer, Function<AbstractInsnNode, Instruction>> factories = new HashMap<>();
 
     static {
-        register(0x19, node -> new ALoad(((VarInsnNode) node).var));
+        register(node -> new Load(((VarInsnNode) node).var), Load.OP_CORES);
+        register(node -> new Store(((VarInsnNode) node).var), Store.OP_CORES);
+        register(node -> new Push<>(HPrimitiveType.BYTE, (byte) ((IntInsnNode) node).operand), 0x10);
+        register(() -> new Compare(CompareOperation.LESS_EQUALS), 0xa4);
+        register(ReturnVoid::new, 0xb1);
+        register(Label::new, 0xffffffff);
     }
 
     public static Instruction create(AbstractInsnNode node) {
         int opCode = node.getOpcode();
         Function<AbstractInsnNode, Instruction> factory = factories.get(opCode);
         if (factory == null) {
-            throw new IllegalStateException(String.format("Unknown instruction opcode=%d, type=%s", opCode, node.getClass().getSimpleName()));
+            throw new IllegalStateException(String.format("Unknown instruction opcode=%s, type=%s", Integer.toHexString(opCode), node.getClass().getSimpleName()));
         }
         return factory.apply(node);
     }
 
-    private static void register(int opCode, Function<AbstractInsnNode, Instruction> factory) {
-        factories.put(opCode, factory);
+    private static void register(Function<AbstractInsnNode, Instruction> factory, int... opCodes) {
+        for (int opCode : opCodes) {
+            factories.put(opCode, factory);
+        }
     }
 
-    private static void register(int opCode, Supplier<Instruction> factory) {
-        register(opCode, node -> factory.get());
+    private static void register(Supplier<Instruction> factory, int... opCodes) {
+        register(node -> factory.get(), opCodes);
     }
 }
